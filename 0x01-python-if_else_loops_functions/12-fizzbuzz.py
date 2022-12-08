44dbd051ae044ac66a69ead5387e262e2439d2f2
Sholawasiu <sholawasiu92@gmail.com>
2022-12-08 07:24:18 -0800

    Insert in

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def fizzbuzz():
     for i in range(1, 101):
         if (i % 5 == 0) and (i % 3 == 0):
@@ -9,4 +8,4 @@ def fizzbuzz():
         elif (i % 5 == 0):
             print('Buzz ', end='')
         else:            
-            print('{} '.format(i), en='')
+            print('{} '.format(i), end='')
