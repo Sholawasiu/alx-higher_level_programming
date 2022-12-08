9fce62924baa0ba8ba71e80bb5241999270e5e92
Sholawasiu <sholawasiu92@gmail.com>
2022-12-08 09:24:28 -0800

    Lists of list = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 
 def print_matrix_integer(matrix=[[]]):
     print('\n'.join(
-        [' '.join(['{:d}'.format(idx)for idx in row])for row in matrix]  
+        [' '.join(['{:d}'.format(idx)for idx in row])for row in matrix]
     ))
