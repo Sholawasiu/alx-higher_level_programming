50178b1910b608f3a1e3ede4ced91f35b21f508f
Sholawasiu <sholawasiu92@gmail.com>
2022-12-21 06:29:43 -0800

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 1-search_replace.py
+
+def search_replace(my_list, search, replace):
+    update_list = my_list[:]
+    for elem in range(len(update_list)):
+        if update_list[elem] == search:
+            update_list[elem] = replace
+    return (update_list)
