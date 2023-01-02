c68b5d76242a9c193142ca7952adc31d4aaa4808
Sholawasiu <sholawasiu92@gmail.com>
2023-01-02 01:20:19 -0800

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,7 +3,6 @@
 """Define a class Square."""
 
 
-
 class Square:
     """Represent a square."""
     def __init__(self, size=0):
@@ -16,6 +15,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
