b84f2d020a6e5bd497a81d9f2ae552cb5f658e4a
Sholawasiu <sholawasiu92@gmail.com>
2023-01-19 05:20:52 -0800

    String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -8,7 +8,6 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
-
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -17,11 +16,11 @@ class Rectangle:
         self.height = height
 
     @property
-    def width(self):
+    def width(self):        
         """Get/set the width of the Rectangle."""
         return self.__width
 
-    @width.setter
+    @width.setter    
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -51,3 +50,16 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """Return the printable representation of the Rectangle.
+        Represents the rectangle with the # character.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
