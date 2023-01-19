b26e78f7650317236ff399fdafe7f26470457a61
Sholawasiu <sholawasiu92@gmail.com>
2023-01-19 04:54:33 -0800

    Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -14,15 +14,11 @@ class Rectangle:
             height (int): The height of the new rectangle.
         """
         self.width = width
-        self.height = height
-        
-    @property
-    
+        self.height = height        
+
     def width(self):
         """Get/set the width of the Rectangle."""
         return self.__width
-
-    @width.setter
 
     def width(self, value):
         if not isinstance(value, int):
@@ -30,14 +26,10 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
-    @property
     
     def height(self):
         """Get/set the height of the Rectangle."""
         return self.__height
-    
-    @height.setter
     
     def height(self, value):
         if not isinstance(value, int):
