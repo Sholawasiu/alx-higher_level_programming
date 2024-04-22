79170a9d1ed7611adb671ee99ea632ba9d95ba0a
Sholawasiu <sholawasiu92@gmail.com>
2024-04-22 05:11:32 -0700

    return python onj

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Function that returns an object (Python data structure)
+represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Function that returns an object (Python data structure)
+    represented by a JSON string.
+
+    Arg:
+        my_str (string): a string object
+    """
+    return json.loads(my_str)
