4286d78cdf72b188f1420dc51e015c2e7fb19fd2
Sholawasiu <sholawasiu92@gmail.com>
2024-04-22 05:04:09 -0700

    read and count

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,22 @@
+def write_file(filename="", text=""):
+    """
+    Function that writes to a text file (UTF-8) and returns
+    the number of characters written.
+
+    Args:
+        filename (text file): the path to the filename
+        text (string): the content to insert to the file
+
+    Returns:
+        the number of characters written
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.write(text)
+
+    with open(filename, encoding='utf-8') as file:
+        lines = file.readlines()
+        chars = 0
+        for line in lines:
+            chars += len(line)
+
+    return chars
