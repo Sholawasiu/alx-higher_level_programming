9716652bd638486b8a9b81a8f370d5ad42f68839
Sholawasiu <sholawasiu92@gmail.com>
2024-04-22 05:15:14 -0700

    load obj from jason file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function that creates an Object from a “JSON file”.
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Function that creates an Object from a “JSON file”.
+
+    Arg:
+        filename (file): the path to the JSON file.
+    """
+    with open(filename) as file:
+        return json.load(file)
