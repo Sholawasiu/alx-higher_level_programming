f7a69e86e898a8b3b361b82a393dd4ca40068eae
Sholawasiu <sholawasiu92@gmail.com>
2024-05-25 02:10:17 -0700

    Go to UTF-8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- 17. Go to UTF8
+-- Query
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE hbtn_0c_0;
+
+ALTER TABLE first_table CONVERT TO
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
