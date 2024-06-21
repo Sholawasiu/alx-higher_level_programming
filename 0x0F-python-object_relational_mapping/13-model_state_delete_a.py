6bf5cc443fa93ed83b740c15f13a03f418723ddf
Sholawasiu <sholawasiu92@gmail.com>
2024-06-21 09:37:58 -0700

    Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This script adds the State object "Louisiana" to the database hbtn_0e_6_usa
+This script deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy import (create_engine)
@@ -9,17 +10,16 @@ from model_state import Base, State
 
 
 def main():
+    """ Main function. """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)()
-    state = State(name="Louisiana")
-    session.add(state)
+    result = session.query(State).filter(State.name.like('%a%')) \
+        .delete(synchronize_session='fetch')
+
     session.commit()
-
-    print(f"{state.id}")
-
     session.close()
 
 
