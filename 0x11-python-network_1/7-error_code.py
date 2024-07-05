ab0c7fb82a58e90fcfc4b913acebe3cb89c51ef7
Sholawasiu <sholawasiu92@gmail.com>
2024-07-05 11:05:36 -0700

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 7-error_code.py
+"""
+This python script takes in a URL, sends a request to the URL,
+and displays the body of the response
+"""
+import sys
+import requests
+
+
+def main():
+    """ Main function """
+    res = requests.get(sys.argv[1])
+
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
+
+
+if __name__ == '__main__':
+    main()
