2bb0a41f836274564191367cbfb81b04e8d24d32
Sholawasiu <sholawasiu92@gmail.com>
2024-07-05 10:51:59 -0700

    What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 4-hbtn_status.py
+"""
+This python script fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+def main():
+    """ Main function """
+    res = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
+
+
+if __name__ == '__main__':
+    main()
